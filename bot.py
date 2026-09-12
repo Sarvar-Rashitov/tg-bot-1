a0030d07084750f4aabcebcd70fed62543008306
Sarvar <sarvarrashitov4321@gmail.com>
2026-09-12 17:07:21 +0500

    photo, video, audio

--- bot.py
+++ bot.py
@@ -1,5 +1,11 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import (
+    Application, 
+    CommandHandler, 
+    ContextTypes,
+    MessageHandler,
+    filters
+) 
 import env
 
 
@@ -64,12 +70,92 @@ async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text("Biz IPE School o'quvchilarimiz")
 
+
+
+async def get_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update.effective_user)
+
+    text = update.message.text      # text = "Salom"
+
+    await update.message.reply_text(f"Xabaringizni oldim. \nQuyidagi xabarni yozdingiz: \n{text}")
+
+
+async def get_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update.effective_user)
+
+    photo = update.message.photo[-1]
+
+    photo_id = photo.file_id
+
+    username = update.effective_user.username
+
+    file = await context.bot.get_file(photo_id)
+
+    filename = f"photos/{username}.jpg"
+
+    await file.download_to_drive(filename)
+
+    await update.message.reply_photo(photo_id)
+
+
+
+
+async def get_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update.effective_user)
+
+    video = update.message.video
+
+    video_id = video.file_id
+
+    username = update.effective_user.username
+
+    file = await context.bot.get_file(video_id)
+
+    filename = f"videos/{username}.mp4"
+
+    await file.download_to_drive(filename)
+
+    await update.message.reply_video(video_id)
+
+
+
+
+async def get_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update.effective_user)
+
+    audio = update.message.audio
+
+
+    audio_id = audio.file_id
+
+    name = audio.file_name
+
+    file = await context.bot.get_file(audio_id)
+
+    filename = f"audios/{name}"
+
+    await file.download_to_drive(filename)
+
+    await update.message.reply_audio(audio_id)
+
+
+
+
 app = Application.builder().token(env.BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("info", get_info))
 app.add_handler(CommandHandler("help", get_help))
 app.add_handler(CommandHandler("about", about))
+
+app.add_handler(MessageHandler(filters.TEXT, get_text))
+app.add_handler(MessageHandler(filters.PHOTO, get_photo))
+app.add_handler(MessageHandler(filters.VIDEO, get_video))
+app.add_handler(MessageHandler(filters.AUDIO, get_audio))
+
+
+
+
 
 
 print("Bot ishga tushdi...")
